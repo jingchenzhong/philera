4b1ecd89db78b27c7a9bf8eea59d65e59caef670
nicephil <nicephil@gmail.com>
2011-11-24 04:55:10 +0800

    libs

--- app/shell/search_rpm.sh
+++ app/shell/search_rpm.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#PROXY="http://10.144.1.10:8080"
+
 URI="http://rpmfind.net/linux/rpm2html/search.php?query="$1"&\
 submit=Search+...&system=&arch=1"
 
-USER_AGENT="Opera/9.80 (X11; Linux i686; U; en) Presto/2.9.168 \
-Version/11.52"
-
-curl $URI > tmp.html
+#USER_AGENT="Opera/9.80 (X11; Linux i686; U; en) Presto/2.9.168 \
+#Version/11.52"
+#export http_proxy=$PROXY
+curl -s $URI > tmp.html
 
 elinks  tmp.html
 
