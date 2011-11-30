b5259dba7417bae633371879a994f3eddf463913
nicephil <nicephil@gmail.com>
2011-11-30 23:50:01 +0800

    auto monitor ag au diagram

--- env/priv/bin/monitor_ag_au.sh
+++ env/priv/bin/monitor_ag_au.sh
@@ -1,3 +1,5 @@
-feh -Z -R 50 -B black http://photo.zhijinwang.com/cn/metals/silver/t24_ag_cny_gram_450x275g.gif 
+#!/bin/bash
 
-#http://photo.zhijinwang.com/cn/metals/gold/t24_au_cny_gram_450x275g.gif
+feh -z -R 50 -B black http://photo.zhijinwang.com/cn/metals/silver/t24_ag_cny_gram_450x275g.gif &
+
+feh -z -R 50 -B black http://photo.zhijinwang.com/cn/metals/gold/t24_au_cny_gram_450x275g.gif &
