018641302ec37ebd9375a28f6db9f5ab401de38e
nicephil <nicephil@gmail.com>
2011-12-01 01:10:21 +0800

    update say.sh

---
+++ env/priv/bin/say.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mplayer -really-quiet "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$1" |:
--- env/priv/bin/speak.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -q -U Mozilla -O /tmp/output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$1" && mplayer -quiet /tmp/output.mp3
