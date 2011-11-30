1025f7a99968e62c3494544c7e1c532ca523c544
nicephil <nicephil@gmail.com>
2011-12-01 01:16:49 +0800

    update say.sh

--- env/priv/bin/say.sh
+++ env/priv/bin/say.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mplayer -really-quiet "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$1" |:
+mplayer -really-quiet "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=$1" &> /dev/null
