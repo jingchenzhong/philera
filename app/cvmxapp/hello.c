d3d92064c2273b2f70d08b479a867f1a8e19cf35
nicephil <nicephil@gmail.com>
2011-12-09 12:03:20 +0800

    ad

--- app/cvmxapp/hello.c
+++ app/cvmxapp/hello.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <cvmx.h>
 #include <cvmx-flash.h>
+#include <cvmx-sysinfo.h>
 
 int main(void)
 {
 
     cvmx_flash_initialize();
     void *flash_ptr = cvmx_flash_get_base(0);
-    printf ("0x%016llx\n", (unsigned long long)flash_ptr);
     printf("\n");
-    printf("Hello world!\n");
+    printf("Hello world! - %s\n", __TIME__);
 
     printf("Hello example run successfully.\n");
     return 0;
