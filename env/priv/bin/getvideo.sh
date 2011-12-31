173996837b923fec586b1947546a7070d3b76fdb
nicephil <nicephil@gmail.com>
2011-12-31 12:51:53 +0800

    adjust wget paramters

--- env/priv/bin/getvideo.sh
+++ env/priv/bin/getvideo.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 
-if (( $# < 1 ))
+if (( $# < 2 ))
 then
-    echo "Usage: $0 <url from youku>"
+    echo "Usage: $0 <url from youku> <dest path>"
     exit -1
 fi
 web_url=`echo $1 | sed -n 's/:/%3A/g;s/\//%2F/gp'` 
 
 errcode=1
+if [[ -d $2 ]]
+then
+    TPATH=$2
+else
+    TPATH=.
+    echo 'wrong path, so use pwd'
+fi
+
 
 # get url address
-curl -s -A 'Mozilla/4.0' "http://www.flvcd.com/parse.php?kw=$web_url&flag=&format=" |\
-    sed -n '/getFlvPath/s/^.*\(http[^ ]*\).*$/\1/p' > url.log 
+curl -s -A 'Mozilla/4.0' "http://www.flvcd.com/parse.php?kw=$web_url&flag=&format=" | \
+sed -n -e '/getFlvPath/s/^.*\(http[^ ]*\).*$/\1/p' | sed -e '/\.\.\.\./d' -e '//d' > $TPATH/url.log 
 
 
 errcode=$?
 if (( $errcode != 0 ))
 then
-    rm -rvf url.log
+    rm -rvf $TPATH/url.log
     echo "failed"
     exit $errcode
 fi
+
+# generate playlist
+sed -n 's/^.*fileid\/\([^ ?=]*\).*$/\1.flv/p' $TPATH/url.log > $TPATH/all.playlist
+mv $TPATH/url.log $TPATH/all_url.playlist
+
 
 # download video from real site 
 errcode=1
 while (( $errcode != 0 ))
 do
-   aria2c -d . -c -i url.log
+   #aria2c -d $TPATH -c -i $TPATH/all_url.playlist
+   wget --user-agent="Mozilla/4.0" -P $TPATH -i $TPATH/all_url.playlist
+
    errcode=$?
 done
 
 
-# generate playlist
-date=`date +%Y%m%d%H%M%S`
-sed -n 's/^.*fileid\/\([^ ?=]*\).*$/\1.flv/p' url.log > all_$date.playlist
-
-mv url.log all_url_$date.playlist
 
 exit 0
 
