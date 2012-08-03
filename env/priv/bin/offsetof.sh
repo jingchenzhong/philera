8c0a23959a81cb00d0dfb450c2d0a82eec14999c
nicephil <nicephil@gmail.com>
2012-08-03 15:30:20 +0800

    fix bug offsetof

--- env/priv/bin/offsetof.sh
+++ env/priv/bin/offsetof.sh
@@ -11,7 +11,7 @@ fi
 dwarf_file=`basename $1`.dwarf
 rm -f $dwarf_file
 
-${CROSS_COMPILE}objdump -W $1 > $dwarf_file
+${CROSS_COMPILE}objdump -Wi $1 > $dwarf_file
 if (( $? != 0 ))
 then
     echo "ELF file could not be recongnized by ${CROSS_COMPILE}objdump"
@@ -20,8 +20,8 @@ then
 fi
 
 
-isstruct=0
-ismember=0
+declare -i isstruct=0
+declare -i ismember=0
 struct_name="ANONYMOUS"
 member_name="ANONYMOUS"
 struct_size=0
@@ -30,8 +30,15 @@ member_offset=0
 
 while read line_str
 do
+
     if [[ $line_str == *"(DW_TAG_structure_type)"* ]]
     then
+        struct_name="ANONYMOUS"
+        member_name="ANONYMOUS"
+        struct_size=0
+        member_offset=0
+
+        ismember=0
         isstruct=1
         continue
     fi
@@ -39,57 +46,49 @@ do
     if (( $isstruct ))
     then
 
-        if [[ $line_str =~ "DW_AT_name" ]]
+        if [[ $line_str == *"DW_AT_name"* ]]
         then
-            echo ""
-            echo "/* struct ${line_str##*: }; */"
             struct_name=`tr [a-z] [A-Z] <<< "${line_str##*: }"`
             continue
         fi
 
-        if [[ $line_str =~ "DW_AT_byte_size" ]]
+        if [[ $line_str == *"DW_AT_byte_size"* ]]
         then
             struct_size=${line_str##*: }
+            echo ""
+            echo "/* struct `tr [A-Z] [a-z] <<< $struct_name`; */"
             echo "#define T_${struct_name}_SIZE ${struct_size}"
-            continue
-        fi
-
-        if [[ $line_str == *"(DW_TAG_member)"* ]]
-        then
-            ismember=1
             isstruct=0
             continue
         fi
+
+    fi
+
+    if [[ $line_str == *"(DW_TAG_member)"* ]]
+    then
+        member_name="ANONYMOUS"
+        member_offset=0
+        ismember=1
+        continue
     fi
 
     if (( $ismember ))
     then
-       if [[ $line_str =~ "DW_AT_name" ]]
+       if [[ $line_str == *"DW_AT_name"* ]]
         then
             member_name=`tr [a-z] [A-Z] <<< "${line_str##*: }"`
             continue
         fi
 
-        if [[ $line_str =~ "DW_OP_plus_uconst" ]]
+        if [[ $line_str == *"DW_OP_plus_uconst"* ]]
         then
             member_offset=${line_str##*: }
             member_offset=${member_offset%)*}
             echo "#define T_${struct_name}_${member_name}_OFFSET ${member_offset}"
+            ismember=0
             continue
         fi
 
-    fi
-
-
-    if [[ $line_str =~ "^ <1>" ]]
-    then
-        isstruct=0
-        ismember=0
-        struct_name="ANONYMOUS"
-        member_name="ANONYMOUS"
-        struct_size=0
-        member_offset=0
-        continue
     fi
 
 done < $dwarf_file
