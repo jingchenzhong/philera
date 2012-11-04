8b1cbaa3bff532cb94393dd492dae6db4e02d873
nicephil <nicephil@gmail.com>
2012-11-04 19:23:22 +0800

    systemd

--- env/priv/bin/enable_wifi.sh
+++ env/priv/bin/enable_wifi.sh
@@ -1,6 +1,14 @@
-ip link set wlan0 up
-iwconfig wlan0 mode ad-hoc
-iwconfig wlan0 channel 11 essid $(perl -e 'print "\xc3\xdb\xb9\xde"') key 's:fuckyougirl!!'
-iwconfig wlan0
-sleep 5
-dhcpcd wlan0
+ping -c 1 192.168.0.1
+ret=$?
+while (( ret !=0 ))
+do
+    ip link set wlan0 down
+    iwconfig wlan0 mode ad-hoc
+    iwconfig wlan0 channel 11 essid $(perl -e 'print "\xc3\xdb\xb9\xde"') key 's:fuckyougirl!!'
+    ip link set wlan0 up
+    sleep 5
+    iwconfig wlan0
+    dhcpcd wlan0
+    ping -c 1 192.168.0.1
+    ret=$?
+done
